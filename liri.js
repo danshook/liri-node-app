4d75d1e1b06472c87aa3cb870615642da74549f7
Space Mac <spacemac@Daniels-MacBook-Pro.local>
2018-04-07 22:15:34 -0700

    tweets are logging

--- liri.js
+++ liri.js
@@ -1,4 +1,28 @@
 require("dotenv").config();
 
+var Twitter = require("twitter");
+var Spotify = require("node-spotify-api");
+var Request = require("request");
+var keys = require("./keys");
+
+var params = { screen_name: "voodoorundown", count: 20 };
 var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+
+function myTweets() {
+  var client = new Twitter(keys.twitter);
+  client.get("statuses/user_timeline", params, function(
+    error,
+    tweets,
+    response
+  ) {
+    if (!error) {
+      for (var i = 0; i < tweets.length; i++) {
+        console.log(
+          "Tweet: " + tweets[i].text + "\nCreated on: " + tweets[i].created_at
+        );
+      }
+    }
+  });
+}
+
+myTweets();
